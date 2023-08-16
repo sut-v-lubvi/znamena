482d445c1b43f838aca372d342e320c4189e7ad5
cipher <sut-v-lubvi@yandex.ru>
2023-08-16 16:11:05 +0300

    create-finish-component

--- src/screens/ScreenFinish/ScreenFinish.tsx
+++ src/screens/ScreenFinish/ScreenFinish.tsx
@@ -1,8 +1,5 @@
-import { connect } from "react-redux";
 import Finish from "../../components/Finish/Finish";
 
 export const ScreenFinish = () => {
   return <Finish />;
 };
-
-export default connect()(ScreenFinish);
